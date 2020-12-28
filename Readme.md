9c3d17f5d1d93be4a3c479a460708a3b27d3229b
MatteoMilani95 <63998279+MatteoMilani95@users.noreply.github.com>
2020-12-28 11:26:32 +0100

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# PCI_o_B
+ DSHpy is a python package developed to Analyze DSH videos computing correlation maps from MIfiles and velocity maps from correlation maps
+ 
+
+## Setup
+ The package can be installed directly from this GitHub repository by calling ```pip install git+https://github.com/MatteoMilani95/PCI_on_B``` and updated using ```pip install --upgrade -e git:https://github.com/MatteoMilani95/PCI_on_B#egg=PCI_on_B```.
+ Check the tutorial included in the package.
---
+++ setup.py
@@ -0,0 +1,29 @@
+"""Setuptools setup script."""
+from setuptools import setup
+
+setup(name='PCI_o_B',
+      version='0.1',
+      description='Analyze speckle fields, compute correlations, derive motion maps',
+      url='https://github.com/MatteoMilani95/PCI_on_B',
+      author='Matteo Milani',
+      author_email='matteo.milani@umontpellier.fr',
+      license='GNU GPL',
+      packages=['PCI_o_B'],
+      install_requires=[
+            'numpy',
+            'scipy',
+            'configparser',
+            'math',
+            'math',
+            'pynverse',
+            'IPython',
+            'pandas',
+            'os'
+      ], 
+      #NOTE: other modules optionally used: emcee
+      #      - emcee (VelMaps)
+      #      - pandas (VelMaps)
+      #      - astropy (PostProcFunctions)
+      #test_suite='nose.collector',
+      #tests_require=['nose'],
+      zip_safe=False)
